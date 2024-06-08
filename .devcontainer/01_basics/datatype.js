0b7e54a33f0fc5272b6d62a7c6e70f5ab544cb84
sujal-kunwar <kunwarsujal821@gmail.com>
2024-06-08 16:49:42 +0530

    datatype.js

--- .devcontainer/01_basics/datatype.js
+++ .devcontainer/01_basics/datatype.js
@@ -1,1 +1,28 @@
+"use strict"; //treat all js code as newer version
 
+ //we are using nodejs, not browser //alert(3+3) 
+
+// console.log(3 +3);
+
+// console.log("sujal")
+
+//string   //either single or double quoted represent -->string 
+let name = "suajl" 
+
+//number 
+let age= 14
+//boolean
+let logging = true
+
+//null    --> standalone value / empty
+let state = "";   //
+
+//undefined  -->not assined value
+
+let one;
+
+//symbol -->unique identifier
+
+//typesof  -->typesof used to found out values datatypes 
+
+console.log(typeof age);  
